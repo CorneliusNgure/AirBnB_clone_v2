49a91e4b2031de2d36cf84b51ac388b167c7c295
CorneliusNgure <kingconelius07@gmail.com>
2024-03-23 23:01:06 -0400

    Task 5

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage or DBStorage"""
-
-import os
+"""This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 
-storage = None
 
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    storage = FileStorage()
-
+storage = FileStorage()
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,25 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
-
-    @property
-    def cities(self):
-        """Getter attribute that returns the list of City instances with state_id equals to
-        the current State.id"""
-        from models import storage
-        from models.city import City
-        city_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    name = ""
